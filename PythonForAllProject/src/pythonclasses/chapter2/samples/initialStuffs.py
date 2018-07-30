e938f15bfc1fc44b2ce707753e0e63e707b77e43
marcelocastilho <marcelocasnet@gmail.com>
2018-07-30 16:35:36 -0300

    First commit... chapter 2 completed and stating chapter 3

---
+++ PythonForAllProject/src/pythonclasses/chapter2/samples/initialStuffs.py
@@ -0,0 +1,53 @@
+from builtins import str
+
+#
+#Comentarios são assim!! 
+#
+
+#Variables types/assigns  
+msg = 'Hello World'
+isOdd = False
+intOrange = 15
+floatOrange = 3.5
+
+print('The type of varible msg is ', type(msg))
+print('The type of varible isOdd is ', type(isOdd))
+print('The type of varible intOrange is ', type(intOrange))
+print('The type of varible floatOrange is ', type(floatOrange))
+
+#Types Conversions
+print('Float value of intOrange ' , float(intOrange))
+
+print('Int value of floatOrange ' , int(floatOrange))
+
+print('Float value of the string 123 ', float(123))
+
+#Expressions
+floatResultOfAMultiplicationExpression = float(floatOrange * intOrange * (floatOrange - 1))
+print('floatResultOfAMultiplicationExpression = ', floatResultOfAMultiplicationExpression) 
+
+floatResultOfADivisionExpression = intOrange / 2
+print('floatResultOfADivisionExpression = ', floatResultOfADivisionExpression)
+
+floatResultOfAPowerExpression =  floatResultOfAMultiplicationExpression ** floatResultOfADivisionExpression
+print('floatResultOfAPowerExpression = ', floatResultOfAPowerExpression)
+
+print("Printing a convertion into a expression", int(35)*float(2.75) )
+
+
+#Conditional samples
+print('Verifying if the number ' + str(intOrange) + " is ODD")
+if(intOrange % 2 > 0):
+    print('intOrange IS ODD!')
+else:
+    print('intOrange IS Pair!') 
+
+#Simple Loop tests
+loopCount = 10  
+for i in range(0, loopCount):
+    print(i, ' ' + msg)
+    #if ()
+    
+    
+
+    
---
+++ PythonForAllProject/src/pythonclasses/chapter2/samples/inputSample.py
@@ -0,0 +1,11 @@
+
+#Simple example of input 
+name = input('Whats your name?')
+print('Your name is ', name)
+
+#Adding 1 in a received number by input method, needing to convert str to int
+inputValue = input("Please input a number to add one more")
+numberToAddOne = int(inputValue) + 1
+print("The result of sum is ", numberToAddOne)
+
+
